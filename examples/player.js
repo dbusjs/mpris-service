e3f042f80510ac53dc940defd675d86807862c14
Emersion <contact@emersion.fr>
2014-12-07 21:41:19 +0100

    First commit

---
+++ examples/player.js
@@ -0,0 +1,22 @@
+var Player = require('..');
+
+var player = Player({
+	name: 'nodejs',
+	title: 'Node.js media player',
+	supportedUriSchemes: ['file'],
+	supportedMimeTypes: ['audio/mpeg', 'application/ogg']
+});
+
+setTimeout(function () {
+	// @see http://www.freedesktop.org/wiki/Specifications/mpris-spec/metadata/
+	player.metadata = {
+		'mpris:trackid': '0',
+		'mpris:length': 60,
+		//'mpris:artUrl': 'http://www.adele.tv/images/facebook/adele.jpg',
+		'xesam:title': 'Lolol',
+		'xesam:album': '21',
+		'xesam:artist': 'Adele'
+	};
+
+	player.playbackStatus = 'Playing';
+}, 3000);
---
+++ index.js
@@ -0,0 +1,229 @@
+var DBus = require('dbus');
+var events = require('events');
+var util = require('util');
+
+var dbus = new DBus();
+
+function Type(signature, name) {
+	return { type: signature, name: name };
+}
+function lcfirst(str) {
+	return str[0].toLowerCase()+str.substr(1);
+}
+
+function Player(opts) {
+	if (!(this instanceof Player)) return new Player(opts);
+	events.EventEmitter.call(this);
+
+	var that = this;
+
+	this.name = opts.name;
+	this.title = opts.title;
+	this.supportedUriSchemes = opts.supportedUriSchemes;
+	this.supportedMimeTypes = opts.supportedMimeTypes;
+
+	this.position = 0;
+
+	var propertiesList = ['PlaybackStatus', 'LoopStatus', 'Volume', 'Metadata'];
+	var properties = {};
+	var addProperty = function (prop) {
+		Object.defineProperty(that, lcfirst(prop), {
+			get: function () {
+				return properties[prop];
+			},
+			set: function (newValue) {
+				properties[prop] = newValue;
+
+				var changed = {};
+				changed[prop] = newValue;
+				that.obj.propertyInterface.emitSignal('PropertiesChanged', 'org.mpris.MediaPlayer2.Player', changed, []);
+			},
+			enumerable: true,
+			configurable: true
+		});
+	};
+	for (var i = 0; i < propertiesList.length; i++) {
+		addProperty(propertiesList[i]);
+	}
+
+	this.init();
+}
+util.inherits(Player, events.EventEmitter);
+
+Player.prototype.init = function () {
+	// Create a new service, object and interface
+	this.serviceName = 'org.mpris.MediaPlayer2.'+this.name;
+	this.service = dbus.registerService('session', this.serviceName);
+	this.obj = this.service.createObject('/org/mpris/MediaPlayer2');
+
+	// TODO: must be defined in dbus module (pull request pending)
+	this.obj.propertyInterface.addSignal('PropertiesChanged', {
+		types: [Type('s', 'interface_name'), Type('a{sv}', 'changed_properties'), Type('as', 'invalidated_properties')]
+	});
+	this.obj.propertyInterface.update();
+
+	this.interfaces = {};
+	this._createRootInterface();
+	this._createPlayerInterface();
+};
+
+/**
+ * @see http://specifications.freedesktop.org/mpris-spec/latest/Media_Player.html
+ */
+Player.prototype._createRootInterface = function () {
+	var that = this;
+	var iface = this.obj.createInterface('org.mpris.MediaPlayer2');
+	iface.addMethod('Raise', {}, function () {
+		that.emit('raise');
+	});
+	iface.addMethod('Quit', {}, function () {
+		that.emit('quit');
+	});
+
+	iface.addProperty('CanQuit', {
+		type: Type('b'),
+		getter: function(callback) {
+			callback(true);
+		}
+	});
+	iface.addProperty('CanRaise', {
+		type: Type('b'),
+		getter: function(callback) {
+			callback(true);
+		}
+	});
+	iface.addProperty('HasTrackList', {
+		type: Type('b'),
+		getter: function(callback) {
+			callback(false);
+		}
+	});
+	iface.addProperty('Identity', {
+		type: Type('s'),
+		getter: function(callback) {
+			callback(that.title);
+		}
+	});
+	/*iface.addProperty('DesktopEntry', {
+		type: Type('s'),
+		getter: function(callback) {
+			callback('lol');
+		}
+	});*/
+	iface.addProperty('SupportedUriSchemes', {
+		type: Type('as'),
+		getter: function(callback) {
+			callback(that.supportedUriSchemes || []);
+		}
+	});
+	iface.addProperty('SupportedMimeTypes', {
+		type: Type('as'),
+		getter: function(callback) {
+			callback(that.supportedMimeTypes || []);
+		}
+	});
+
+	iface.update();
+	this.interfaces.root = iface;
+};
+
+/**
+ * @see http://specifications.freedesktop.org/mpris-spec/latest/Player_Interface.html
+ */
+Player.prototype._createPlayerInterface = function () {
+	var that = this;
+	var iface = this.obj.createInterface('org.mpris.MediaPlayer2.Player');
+
+	var eventMethods = ['Next', 'Previous', 'Pause', 'PlayPause', 'Stop', 'Play'];
+	var addEventMethod = function (method) {
+		iface.addMethod(method, {}, function () {
+			that.emit(lcfirst(method));
+		});
+	};
+	for (var i = 0; i < eventMethods.length; i++) {
+		addEventMethod(eventMethods[i]);
+	}
+
+	iface.addMethod('Seek', { in: [ Type('x') ] }, function (delta) {
+		console.log('Seek', delta);
+		that.emit('seek', { delta: delta, position: that.position + delta });
+	});
+	iface.addMethod('SetPosition', { in: [ Type('o'), Type('x') ] }, function (trackId, pos) {
+		console.log('SetPosition', trackId, pos);
+		that.emit('position', { trackId: trackId, position: pos });
+	});
+	iface.addMethod('OpenUri', { in: [ Type('s') ] }, function (uri) {
+		console.log('OpenUri', uri);
+		that.emit('open', { uri: uri });
+	});
+
+	iface.addProperty('PlaybackStatus', {
+		type: Type('s'),
+		getter: function(callback) {
+			callback(that.playbackStatus || 'Stopped');
+		}
+	});
+	iface.addProperty('LoopStatus', {
+		type: Type('s'),
+		getter: function(callback) {
+			callback(that.loopStatus || 'None');
+		}
+	});
+	iface.addProperty('Metadata', {
+		type: Type('a{sv}'),
+		getter: function(callback) {
+			callback(that.metadata || {});
+		}
+	});
+	iface.addProperty('Volume', {
+		type: Type('d'),
+		getter: function(callback) {
+			callback(that.volume || 1);
+		}
+	});
+	iface.addProperty('Position', {
+		type: Type('x'),
+		getter: function(callback) {
+			callback(that.position || 0);
+		}
+	});
+
+	iface.addProperty('CanPlay', {
+		type: DBus.Define(Boolean),
+		getter: function(callback) {
+			callback(true);
+		}
+	});
+	iface.addProperty('CanPause', {
+		type: DBus.Define(Boolean),
+		getter: function(callback) {
+			callback(true);
+		}
+	});
+	iface.addProperty('CanSeek', {
+		type: DBus.Define(Boolean),
+		getter: function(callback) {
+			callback(true);
+		}
+	});
+	iface.addProperty('CanControl', {
+		type: DBus.Define(Boolean),
+		getter: function(callback) {
+			callback(true);
+		}
+	});
+
+	iface.addSignal('Seeked', {
+		types: [Type('x')]
+	});
+
+	iface.update();
+	this.interfaces.player = iface;
+};
+
+Player.prototype.seeked = function (delta) {
+	this.position += delta || 0;
+	this.interfaces.player.emitSignal('Seeked', this.position);
+};
+
+module.exports = Player;
