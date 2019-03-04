3750ddf37b9ca894f5659f1358ce6cef0bad6aa6
Tony Crisci <tony@dubstepdish.com>
2019-03-03 19:54:13 -0500

    add documentation

--- src/index.js
+++ src/index.js
@@ -17,6 +17,66 @@ function lcfirst(str) {
   return str[0].toLowerCase()+str.substr(1);
 }
 
+/**
+ * Construct a new Player and export it on the DBus session bus.
+ *
+ * For more information about the properties of this class, see [the MPRIS DBus Interface Specification](https://specifications.freedesktop.org/mpris-spec/latest/).
+ *
+ * Method Call Events
+ * ------------------
+ *
+ * The Player is an `EventEmitter` that emits events when the corresponding
+ * methods are called on the DBus interface over the wire.
+ *
+ * The Player emits events whenever the corresponding methods on the DBus
+ * interface are called.
+ *
+ * * `raise` - Brings the media player's user interface to the front using any appropriate mechanism available.
+ * * `quit` - Causes the media player to stop running.
+ * * `next` - Skips to the next track in the tracklist.
+ * * `previous` - Skips to the previous track in the tracklist.
+ * * `pause` - Pauses playback.
+ * * `playPause` - Pauses playback.  If playback is already paused, resumes playback. If playback is stopped, starts playback.
+ * * `stop` - Stops playback.
+ * * `play` - Starts or resumes playback.
+ * * `seek` - Seeks forward in the current track by the specified number of microseconds. With event data `{ delta, position }`.
+ * * `position` - Sets the current track position in microseconds. With event data `{ trackId, position }`.
+ * * `open` - Opens the Uri given as an argument. With event data `{ uri }`.
+ * * `activatePlaylist` -  Starts playing the given playlist. With event data `playlistId`.
+ *
+ *
+ * @class Player
+ * @param {Object} options - Options for the player
+ * @param {String} options.name - Name on the bus to export to as `org.mpris.MediaPlayer2.{name}`.
+ * @param {String} options.identity - Identity for the player to display on the root media player interface.
+ * @param {Array} options.supportedMimeTypes - Mime types this player can open with the `org.mpris.MediaPlayer2.Open` method.
+ * @param {Array} options.supportedInterfaces - The interfaces this player supports. Can include `'player'`, `'playlists'`, and `'trackList'`.
+ * @property {String} identity - A friendly name to identify the media player to users.
+ * @property {Boolean} fullscreen - Whether the media player is occupying the fullscreen.
+ * @property {Array} supportedUriSchemes - The URI schemes supported by the media player.
+ * @property {Array} supportedMimeTypes - The mime-types supported by the media player.
+ * @property {Boolean} canQuit - Whether the player can quit.
+ * @property {Boolean} canRaise - Whether the player can raise.
+ * @property {Boolean} canSetFullscreen - Whether the player can be set to fullscreen.
+ * @property {Boolean} hasTrackList - Indicates whether the /org/mpris/MediaPlayer2 object implements the org.mpris.MediaPlayer2.TrackList interface.
+ * @property {String} desktopEntry - The basename of an installed .desktop file which complies with the Desktop entry specification, with the ".desktop" extension stripped.
+ * @property {String} playbackStatus - The current playback status. May be "Playing", "Paused" or "Stopped".
+ * @property {String} loopStatus - The current loop/repeat status. May be "None", "Track", or "Playlist".
+ * @property {Boolean} shuffle - Whether the player is shuffling.
+ * @property {Object} metadata - The metadata of the current element. If there is a current track, this must have a "mpris:trackid" entry (of D-Bus type "o") at the very least, which contains a D-Bus path that uniquely identifies this track.
+ * @property {Double} volume - The volume level.
+ * @property {Boolean} canControl - Whether the media player may be controlled over this interface.
+ * @property {Boolean} canPause - Whether playback can be paused using Pause or PlayPause.
+ * @property {Boolean} canPlay - Whether playback can be started using Play or PlayPause.
+ * @property {Boolean} canSeek - Whether the client can control the playback position using Seek and SetPosition.
+ * @property {Boolean} canGoNext - Whether the client can call the Next method on this interface and expect the current track to change.
+ * @property {Boolean} canGoPrevious - Whether the client can call the Previous method on this interface and expect the current track to change.
+ * @property {Double} rate - The current playback rate.
+ * @property {Double} minimumRate - The minimum value which the Rate property can take.
+ * @property {Double} maximumRate - The maximum value which the Rate property can take.
+ * @property {Array} playlists - The current playlists set by Player#setPlaylists. (Not a DBus property).
+ * @property {Integer} activePlaylist - The currently-active playlist.
+ */
 function Player(opts) {
   if (!(this instanceof Player)) {
     return new Player(opts);
@@ -63,7 +123,7 @@ Player.prototype._addPlayerInterface = function(bus) {
   this.interfaces.player = new PlayerInterface(this);
   let eventedProps = ['PlaybackStatus', 'LoopStatus', 'Rate', 'Shuffle',
     'Metadata', 'Volume', 'CanControl', 'CanPause', 'CanPlay', 'CanSeek',
-    'CanGoNext', 'CanGoPrevious', 'MinimumRate', 'MaximumRate', 'Rate'];
+    'CanGoNext', 'CanGoPrevious', 'MinimumRate', 'MaximumRate'];
   this._addEventedPropertiesList(this.interfaces.player, eventedProps);
   bus.export(this.serviceName, MPRIS_PATH, this.interfaces.player);
 };
@@ -94,6 +154,15 @@ Player.prototype._addPlaylistsInterface = function(bus) {
   bus.export(this.serviceName, MPRIS_PATH, this.interfaces.playlists);
 }
 
+/**
+ * Get a valid object path with the `subpath` as the basename which is suitable
+ * for use as an id.
+ *
+ * @name Player#objectPath
+ * @function
+ * @param {String} subpath - The basename of this path
+ * @returns {String} - A valid object path that can be used as an id.
+ */
 Player.prototype.objectPath = function(subpath) {
   let path = `/org/node/mediaplayer/${this.name}`;
   if (subpath) {
@@ -131,6 +200,14 @@ Player.prototype._addEventedPropertiesList = function(iface, props) {
   }
 };
 
+/**
+ * Sets the position of the player to `position + delta` and emits the `Seeked`
+ * DBus signal to listening clients.
+ *
+ * @name Player#seeked
+ * @function
+ * @param {Integer} delta - The change in position in microseconds.
+ */
 Player.prototype.seeked = function(delta) {
   this.position += delta || 0;
   this.interfaces.player.Seeked(this.position);
@@ -140,7 +217,7 @@ Player.prototype.getTrackIndex = function(trackId) {
   for (let i = 0; i < this.tracks.length; i++) {
     let track = this.tracks[i];
 
-    if (track['mpris:trackid'] == trackId) {
+    if (track['mpris:trackid'] === trackId) {
       return i;
     }
   }
@@ -171,6 +248,14 @@ Player.prototype.removeTrack = function(trackId) {
   that.interfaces.tracklist.TrackRemoved(trackId);
 };
 
+/**
+ * Get the index of a playlist entry in the list of Player#playlists from the
+ * given id.
+ *
+ * @name Player#getPlaylistIndex
+ * @function
+ * @param {String} playlistId - The id for the playlist entry.
+ */
 Player.prototype.getPlaylistIndex = function(playlistId) {
   for (let i = 0; i < this.playlists.length; i++) {
     let playlist = this.playlists[i];
@@ -183,6 +268,14 @@ Player.prototype.getPlaylistIndex = function(playlistId) {
   return -1;
 };
 
+/**
+ * Set the list of playlists advertised to listeners on the bus. Each playlist
+ * must have string members `Id`, `Name`, and `Icon`.
+ *
+ * @name Player#setPlaylists
+ * @function
+ * @param {Array} playlists - A list of playlists.
+ */
 Player.prototype.setPlaylists = function(playlists) {
   this.playlists = playlists;
   this.playlistCount = playlists.length;
@@ -193,6 +286,14 @@ Player.prototype.setPlaylists = function(playlists) {
   });
 };
 
+/**
+ * Set the playlist identified by `playlistId` to be the currently active
+ * playlist.
+ *
+ * @name Player#setActivePlaylist
+ * @function
+ * @param {String} playlistId - The id of the playlist to activate.
+ */
 Player.prototype.setActivePlaylist = function(playlistId) {
   this.interfaces.playlists.setActivePlaylistId(playlistId);
 };
