9f8fa8ffbdeccf9ec8b740e79b7619d59712fb7f
Tony Crisci <tony@dubstepdish.com>
2019-03-28 13:16:23 -0400

    Use the Peer interface of the player to ping
    This simplifies the tests a bit.

--- test/fixtures.js
+++
@@ -1,4 +0,0 @@
-module.exports.ping = async function(bus) {
-  let busObj = await bus.getProxyObject('org.freedesktop.DBus', '/org/freedesktop/DBus');
-  return busObj.getInterface('org.freedesktop.DBus').ListNames();
-}
